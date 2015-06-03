55923ecce7889f04f2a913f19b34d6965d0dd5ab
MichaelPhoenix <mykhaylo.yusko@gmail.com>
2015-06-04 01:37:17 +0300

    Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 package customersearch;
 
+
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -7,13 +9,20 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
-public class Main {
+public class Main  {
     public static void main(String[] args)throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String name;
         int sum;
         int opt = 0;
-        int ss;
+        int changeSum;
+        int telephone;
+        String address;
+        HashMap<String,Integer> info = new HashMap<>();
+        Set<Map.Entry<String,Integer>> infoset =  info.entrySet();
+
+
+
         HashMap<String,Integer> map = new HashMap<>();
         Set<Map.Entry<String,Integer>> set = map.entrySet();
         do {
@@ -23,6 +32,8 @@ public class Main {
             System.out.println("3.To delete Customer");
             System.out.println("4.To add sum on account client");
             System.out.println("5.View all customers");
+            System.out.println("6.Enter customer data");
+            System.out.println("7.View customer data");
             System.out.println("0.Exit");
 
 
@@ -61,8 +72,8 @@ public class Main {
                             System.out.print("Enter sum: ");
                             sum = Integer.parseInt(br.readLine());
                             int s = su.getValue();
-                            ss = s + sum;
-                            map.put(name,ss);
+                            changeSum = s + sum;
+                            map.put(name,changeSum);
                         }
                     }
                     break;
@@ -73,6 +84,34 @@ public class Main {
                         System.out.println("Customer: " + vi.getKey());
                     }
                     break;
+
+
+                case 6:
+                    System.out.println("Enter your name: ");
+                    name = br.readLine();
+                    for(Map.Entry<String,Integer> en : set){
+                        if(en.getKey().equals(name)){
+                            System.out.println("Enter address and mobile telephone:  ");
+                            address = br.readLine();
+                            telephone = Integer.parseInt(br.readLine());
+                            info.put(address,telephone);
+                        }
+                    }
+                    break;
+                case 7:
+                    System.out.println("Enter name: ");
+                    name = br.readLine();
+                    for(Map.Entry<String,Integer> view : set){
+                        if(view.getKey().equals(name)){
+                            for(Map.Entry<String,Integer> inf : infoset){
+                                System.out.println("Adrres: " + inf.getKey());
+                                System.out.println("Telephone: " + inf.getValue());
+                            }
+                        }
+                    }
+                    break;
+
+
 
                 case 0:
                     System.out.println("Thank you!!\n****Good Bye****");
